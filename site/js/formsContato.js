d2e8a7a9d8859c93c06c4f3a541665aeeb7d1102
Matheus Santana <matheus.santana7@hotmail.com>
2024-09-12 01:23:19 -0300

    resolvendo bug do forms

--- site/js/formsContato.js
+++ site/js/formsContato.js
@@ -1,23 +1,24 @@
-function sendEmail() {
-    var params = {
-        nome: document.getElementById("nomeid").value,
-        telefone: document.getElementById("foneid").value,
-        email: document.getElementById("email").value,
-        descricao: document.getElementById("text-description").value
-    };
+// function sendEmail() {
+//     var params = {
+//         nome: document.getElementById("nomeid").value,
+//         telefone: document.getElementById("foneid").value,
+//         email: document.getElementById("email").value,
+//         descricao: document.getElementById("text-description").value
+//     };
 
-    emailjs.send("your-service-id", "your-template-id", params)
-        .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-            alert("E-mail enviado com sucesso!");
-        }, function(error) {
-            console.log('FAILED...', error);
-            alert("Falha ao enviar e-mail.");
-        });
-}
+//     emailjs.send("your-service-id", "your-template-id", params)
+//         .then(function(response) {
+//             console.log('SUCCESS!', response.status, response.text);
+//             alert("E-mail enviado com sucesso!");
+//         }, function(error) {
+//             console.log('FAILED...', error);
+//             alert("Falha ao enviar e-mail.");
+//         });
+// }
+
 
 function sendWhatsApp() {
-    var telefone = "seu_numero_com_codigo_do_pais"; // Exemplo: '5511912345678'
+    var telefone = '55' + document.getElementById('foneid').value;
     var mensagem = "Você recebeu uma nova mensagem de: " + document.getElementById("nomeid").value;
     mensagem += " - Telefone: " + document.getElementById("foneid").value;
     mensagem += " - E-mail: " + document.getElementById("email").value;
