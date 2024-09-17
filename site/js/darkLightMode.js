2aa168633b386632c95cf9df7536f943b589d165
Matheus Santana <matheus.santana7@hotmail.com>
2024-09-17 00:31:13 -0300

    finalizando toggle

--- site/js/darkLightMode.js
+++ site/js/darkLightMode.js
@@ -43,10 +43,10 @@ function changeModeLigth()
     document.body.style.color = "black";
     navBar.style.backgroundColor = "white";
     navbarH1.style.borderRight = "2px solid black"
-    // prevIcon.style.backgroundColor = 'black';
-    // nextIcon.style.backgroundColor = 'black';
 
-    prevIcon.style.backgroundImage = `url(data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%000000'%3e%3cpath d='M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z'/%3e%3c/svg%3e)`;
+    prevIcon.style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23000'%3e%3cpath d='M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z'/%3e%3c/svg%3e")`;
+    nextIcon.style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23000'%3e%3cpath d='M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z'/%3e%3c/svg%3e")`;
+    
     btnDownloadAndSend.forEach(element => {
         element.style.color = "black";
     });
@@ -64,6 +64,8 @@ function changeModeDark()
     document.body.style.color = "";
     navBar.style.backgroundColor = "";
     navbarH1.style.borderRight = ""
+    prevIcon.style.backgroundImage = "";
+    nextIcon.style.backgroundImage = "";
 
 
     Inputs.forEach(element => {
