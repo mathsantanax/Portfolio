74e4b0504885a07ff7b322eecfae0ed6226b4d1a
Matheus Santana <matheus.santana7@hotmail.com>
2024-09-18 00:07:58 -0300

    finalizando o formcontato

--- site/js/formsContato.js
+++ site/js/formsContato.js
@@ -1,29 +1,47 @@
-function sendEmail() {
-    var params = {
+    function sendEmail() {
+        var params = {
         nome: document.getElementById("nomeid").value,
         telefone: document.getElementById("foneid").value,
         email: document.getElementById("email").value,
         descricao: document.getElementById("text-description").value
-    };
-
-    emailjs.send("your-service-id", "template_bkf7ybb", params)
-        .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
+        };
+    
+        var templateParams = {
+        'to_name': 'EmailJS team',
+        'from_name': params.nome,
+        'message': 'Telefone: ' + params.telefone + '\nE-mail: ' + params.email + '\nDescrição: ' + params.descricao
+        };
+    
+        
+        var serviceId = 'service_4k6gi4l';
+        var templateId = 'template_bkf7ybb';
+        var userId = 'dyQzijOU_3fP_aNnn';
+    
+        var data = {
+        'service_id': serviceId,
+        'template_id': templateId,
+        'user_id': userId,
+        'template_params': templateParams
+        };
+    
+        fetch('https://api.emailjs.com/api/v1.0/email/send', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+        })
+        .then(response => {
+        if (response.ok) {
+            console.log('SUCCESS!');
             alert("E-mail enviado com sucesso!");
-        }, function(error) {
-            console.log('FAILED...', error);
+        } else {
+            console.log('FAILED...', response.status, response.statusText);
             alert("Falha ao enviar e-mail.");
+        }
+        })
+        .catch(error => {
+        console.log('FAILED...', error);
+        alert("Falha ao enviar e-mail.");
         });
-}
-
-
-function sendWhatsApp() {
-    var telefone = '55' + document.getElementById('foneid').value;
-    var mensagem = "Você recebeu uma nova mensagem de: " + document.getElementById("nomeid").value;
-    mensagem += " - Telefone: " + document.getElementById("foneid").value;
-    mensagem += " - E-mail: " + document.getElementById("email").value;
-    mensagem += " - Descrição: " + document.getElementById("text-description").value;
-
-    var whatsapp_url = "https://wa.me/" + telefone + "?text=" + encodeURIComponent(mensagem);
-    window.open(whatsapp_url, '_blank');
-}
+    }
